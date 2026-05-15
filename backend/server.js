5d4e027a5aeb72c539dcc0c2c6e4e73957c8ffc3
Manish Taluka <manishtaluka413@gmail.com>
2026-05-15 21:54:23 +0530

    updated vercel url in backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: ["http://localhost:5173", "https://rentwheels.vercel.app"],
+  origin: ["http://localhost:5173", "rent-wheels-9e0gvih9z-manish-s-projects4.vercel.app"],
   credentials: true,
 }));
 
